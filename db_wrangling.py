d57edc8d23252a4fb4d6e0945ac2875c65e2a15e
jmoro0408 <james.moro.11@gmail.com>
2022-05-02 16:46:48 -0700

    Refactored db functions Refactored the file to move the database key functions (query/insert/create etc) into its own class.

--- db_wrangling.py
+++ db_wrangling.py
@@ -1,13 +1,15 @@
 from configparser import ConfigParser
+from mimetypes import init
 from typing import Optional
 
 import psycopg2  # type: ignore
 from psycopg2 import Error  # type: ignore
 
 
-def config(filename: str = "database.ini", section: str = "postgresql") -> dict:
+def read_config(filename: str = "database.ini", section: str = "postgresql") -> dict:
     """
-    reads the database.ini configuration file for database connection.
+    reads the database.ini configuration file for database connection and returns a
+    dictionary of the connection configurationd details.
     """
     # create a parser
     parser = ConfigParser()
@@ -28,37 +30,68 @@ def config(filename: str = "database.ini", section: str = "postgresql") -> dict:
     return db
 
 
-def query_db(query: Optional[str] = None) -> None:
+def connect(params: dict) -> psycopg2.extensions.connection:
     """
-    Opens a connection to the database and runs a provided query.
+    creates a connection to the database.
+    IMPORTANT: Does not automatically close the database connection. This should
+    be handled by subsequent db tools (query, insert, create etc) or by calling the close
+    method of the DataBase class.
     """
     conn = None
     try:
         # read connection parameters
-        params = config()
-
+        params = read_config()
         # connect to the PostgreSQL server
-        print("Connecting to the PostgreSQL database...")
         conn = psycopg2.connect(**params)
-
-        # Create a cursor to perform database operations
-        cursor = conn.cursor()
-        cursor.execute(query)
-        results = cursor.fetchall()
-        print(results)
-
-        conn.commit()
-        conn.close()
-
+        return conn
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
-    finally:
-        if conn:
-            cursor.close()
+
+
+class DataBase:
+    """
+    Main database class that handles all querying, inserting, etc
+    """
+
+    def __init__(self, conn: psycopg2.extensions.connection) -> None:
+        self.init = init
+        self.conn = conn
+        self.cursor = conn.cursor()
+
+    def query(self, query: Optional[str] = None) -> None:
+        """
+        runs and prints a given SQL query on the provided database connection
+        """
+        try:
+            self.cursor.execute(query)
+            results = self.cursor.fetchall()
+            print(results)
+
+        except (Exception, Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+        finally:
+            if conn:
+                self.cursor.close()
+                conn.close()
+                print("PostgreSQL connection is closed")
+
+    def close(self):
+        """
+        Manually closes the database connection. Not typically required as closing should
+        be handled by any querying/inserting/creating etc method.
+        """
+        try:
+            self.cursor.close()
             conn.close()
             print("PostgreSQL connection is closed")
+        except (Exception, Error) as error:
+            print("Error while connecting to PostgreSQL", error)
 
 
 if __name__ == "__main__":
     query = """SELECT * from news_source"""
-    query_db(query=query)
+    connection_params = read_config()
+
+    conn = connect(connection_params)
+    db = DataBase(conn=conn)
+    db.query(query=query)
