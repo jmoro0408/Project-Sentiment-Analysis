a7c0430a1e849ccd4af5503b7f6ba5b1c3e1eb71
jmoro0408 <james.moro.11@gmail.com>
2022-05-03 19:35:48 -0700

    added custom exception

--- scraping/bbc.py
+++ scraping/bbc.py
@@ -1,23 +1,37 @@
 """
 Module to scrap bbc news articles.
+The get_bbs_search_urls function can get a list of bbc search page results for
+a given search term and number of pages. This is just the URL of the actual results
+page, not the article urls.
+
+The parse_search_results function can then take this search results page url to
+get the actual article urls we are interested in.
+
 Module will provide article title, text, date, and url
 """
 # TO DO add func to get all bbc articles from search
 # run through all articles from search func and get title/body
 # add capability to get datetime of article
 
+from re import A
 from typing import Iterable, List
 
 import requests  # type: ignore
 from bs4 import BeautifulSoup as bs  # type: ignore
+import pandas as pd
 
+class PageOutOfRangeError(Exception):
+    """Raised when the the provided search range is out of range"""
+    pass
 
-def get_bbc_search_urls(search_term: str, pages: Iterable = [1]) -> List:
+def get_bbc_search_pages(search_term: str, pages: Iterable = [1]) -> List:
     """
     constructs the bbs search urls for a given search term.
     pages is an iterable that represents the page range to get.
     i.e pages = ranges(1,4) will return the first 3 search pages.
     """
+    if 0 in pages:
+        raise PageOutOfRangeError("Search page range starts at 1")
     search_url = f"https://www.bbc.co.uk/search?q={search_term}&page="
     urls = []
     for page in pages:
@@ -27,7 +41,25 @@ def get_bbc_search_urls(search_term: str, pages: Iterable = [1]) -> List:
     return urls
 
 
-class BBC:
+def get_article_urls_from_search(search_results_urls: List) -> List:
+    """
+    returns links to actual articles from the bbc search page.
+    First gets all the anchor tags for each search results page. It then filters out the most likely
+    articles using the substring.
+    Returns a list of all the article urls.
+    """
+    substring = "bbc.co.uk/news/uk"
+    for search_url in search_results_urls:
+        search_results_content = requests.get(search_url).content
+        soup = bs(search_results_content, "html.parser")
+        links = []
+        for link in soup.find_all("a"):
+            links.append(link.get("href"))
+        links_with_substring = [string for string in links if substring in string]
+    return links_with_substring
+
+
+class BBCArticle:
     """
     main scraping class for BBC articles
     """
@@ -37,6 +69,7 @@ class BBC:
         self.soup = bs(article.content, "html.parser")
         self.body = self.get_body()
         self.title = self.get_title()
+        self.result = {}
 
     def get_body(self) -> str:
         """
@@ -52,13 +85,25 @@ class BBC:
         get the article title
         """
         title_class = "ssrcss-15xko80-StyledHeading e1fj1fc10"
-        return self.soup.find(class_=title_class).text
+        try:
+            return self.soup.find(class_=title_class).text
+        except AttributeError:
+            pass
+
+def bbc_article_pipeline(search_term:str, pages:Iterable = [1]) -> pd.DataFrame:
+    search_results_pages = get_bbc_search_pages(search_term=search_term, pages = pages)
+    article_urls = get_article_urls_from_search(search_results_pages)
+    titles = []
+    bodies = []
+    for article_url in article_urls:
+        bbc_article = BBCArticle(url = article_url)
+        titles.append(bbc_article.title)
+        bodies.append(bbc_article.body)
+    results = pd.DataFrame(list(zip(titles, bodies)),columns =['Title', 'Body']).dropna()
+    results = results.reset_index(drop = True )
+    return results
 
 
 if __name__ == "__main__":
-    URL = "https://www.bbc.com/news/uk-england-london-61093756"
-    parsed = BBC(URL)
-    article_body = parsed.body
-    article_title = parsed.title
-    pages = range(1, 3)
-    print(get_bbc_search_urls("crossrail", pages))
+    results = bbc_article_pipeline(search_term = "crossrail", pages = [1])
+    testvar = 1
