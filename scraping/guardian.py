b279a60a723a48ebdc278645f065d953ca6fa2cf
jmoro0408 <james.moro.11@gmail.com>
2022-05-04 21:28:21 -0700

    Started guardian parsing This will be a little different from bbc. Guardian website has an interactive search which sucks for this stuff. But luckily they also provide an api for developers. This will use the api to get the links to the articles we want, then will scrape similar to bbc.

---
+++ scraping/guardian.py
@@ -0,0 +1,33 @@
+from distutils.command.build import build
+from re import A
+from dotenv import load_dotenv
+import os
+import requests
+
+class GuardianArticle():
+    def __init__(self, search_term:str, api_key:str):
+        self.search_term = search_term
+        self.api_key = api_key
+        self.query = self.build_api_query()
+        self.response = self.get_api_response(self.query)
+
+    def build_api_query(self) -> str:
+        """
+        create a string suitable for querying the guardian api
+        """
+        search_term = self.search_term.replace(" ", "%20")
+        return f"https://content.guardianapis.com/search?q={search_term}&api-key={self.api_key}"
+
+    def get_api_response(self, api_query_url:str) -> requests.models.Response:
+        """
+        get response from guardian api
+        """
+        print(type(requests.get(api_query_url)))
+        return requests.get(api_query_url)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.getenv("GUARDIAN_API_KEY")
+    guardian = GuardianArticle("crossrail", api_key)
+    print(guardian.response.text)
