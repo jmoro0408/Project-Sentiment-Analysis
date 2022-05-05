789987a2947f90966d04f6510f369cfe03649a4d
jmoro0408 <james.moro.11@gmail.com>
2022-05-05 08:22:16 -0700

    Added api class Added a class to handle the guardian api, seperating GuardianArticle class

--- scraping/guardian.py
+++ scraping/guardian.py
@@ -8,16 +8,13 @@ import requests
 from dotenv import load_dotenv
 
 
-class GuardianArticle:
+class GuardianAPI():
     """
-    Main guardian class.
+    class to handle the querying of the guardian api
     """
-
     def __init__(self, search_term: str, api_key: str):
-        self.search_term = search_term
-        self.api_key = api_key
-        self.query = self.build_api_query()
-        self.response = self.get_api_response()
+            self.search_term = search_term
+            self.api_key = api_key
 
     def build_api_query(self) -> str:
         """
@@ -30,11 +27,41 @@ class GuardianArticle:
         """
         get response from guardian api
         """
-        return requests.get(self.query)
+        _query = self.build_api_query()
+        response = requests.get(_query) #should add some exception handling for invalid status codes
+        return response
+
+    def parse_api_response(self, result_number: int) -> dict:
+        """
+        parses the response from the api query. See link for examples:
+        https://open-platform.theguardian.com/explore/
+        This method deliberately only parses one result at a time.
+        The intention here is that this method is called in a loop, parsing each result as required.
+        The argument result_number should be supplied, calling out which specific result is wanted.
+        """
+        _response = self.get_api_response()
+        results_list = _response.json()["response"]["results"]
+        article_results = [result for result in results_list if result["type"]== "article"]
+        result_dict = {"webTitle": article_results[result_number]["webTitle"],
+                         "webUrl": article_results[result_number]["webUrl"],
+                         "webPublicationDate": article_results[result_number]["webPublicationDate"]}
+        return result_dict
+
+
+class GuardianArticle:
+    """
+    class to handle the parsing of individial guardian articles
+    """
+    pass
+
+def guardian_api_pipeline(search_term:str, api_key:str):
+    guardian_api = GuardianAPI("crossrail", API_KEY)
+    results = (guardian_api.parse_api_response(0))
+    print(results)
+
 
 
 if __name__ == "__main__":
     load_dotenv()
     API_KEY = str(os.getenv("GUARDIAN_API_KEY"))
-    guardian = GuardianArticle("crossrail", API_KEY)
-    print(type(guardian.response.text))
+    guardian_api_pipeline("crossrail", API_KEY)
