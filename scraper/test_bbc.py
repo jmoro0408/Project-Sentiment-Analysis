4406b6fb6285e35bd280218111959512fa090566
jmoro0408 <james.moro.11@gmail.com>
2022-05-06 20:10:18 -0700

    added test for pageoutofrange error

--- scraper/test_bbc.py
+++ scraper/test_bbc.py
@@ -1,15 +1,19 @@
 import unittest
 import bbc
 
-class TestBBC(unittest.TestCase):
+class TestBBCFuncs(unittest.TestCase):
 
-    def test_get_bbc_search_pages(self):
+    def test_get_bbc_search_pages_return(self):
         search_term  = "crossrail"
-
         pages = [1]
         result = bbc.get_bbc_search_pages(search_term, pages)
         self.assertIsInstance(result,list)
 
+    def test_get_bbc_search_pages_error(self):
+        search_term  = "crossrail"
+        pages = [0]
+        with self.assertRaises(bbc.PageOutOfRangeError):
+           bbc.get_bbc_search_pages(search_term, pages)
+
 if __name__ == '__main__':
     unittest.main()
-
